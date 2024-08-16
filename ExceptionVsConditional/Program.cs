46aaf928cbd532bcea63438e560f1e7c61e78129
Sadri <Sadri@DESKTOP-G3NDRH0>
2024-08-16 10:52:50 -0700

    Add project files.

---
+++ ExceptionVsConditional/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ExceptionVsConditional;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<ExceptionVsConditionalBenchmark>();
+    }
+
+    public class ExceptionVsConditionalBenchmark
+    {
+        private int Numerator = 10;
+        private int Denominator = 0;
+
+        [Benchmark]
+        public int DivideWithConditional()
+        {
+            if (Denominator == 0)
+            {
+                return -1; // Indicate an error
+            }
+            return Numerator / Denominator;
+        }
+
+        [Benchmark]
+        public int DivideWithException()
+        {
+            try
+            {
+                return Numerator / Denominator;
+            }
+            catch (DivideByZeroException)
+            {
+                return -1; // Indicate an error
+            }
+        }
+    }
+
+}
